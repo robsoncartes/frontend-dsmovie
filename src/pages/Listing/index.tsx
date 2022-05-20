aba377b5e56ddd9292210911caf4bea91fbe05fb
Robson Sousa <robsoncartes@outlook.com>
2022-05-20 00:24:58 -0700

    'feat/class1': MovieCard. Create 'index.tsx' and 'styles.css' files in 'MovieCard', 'MovieScore' and 'MovieStars' directories. Update 'index.tsx' file in 'Navbar' and 'Listing' directories.

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -1,9 +1,53 @@
-import Pagination from "../../components/Pagination";
+import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
 
     return (
-        <Pagination></Pagination>
+        <>
+            <Pagination></Pagination>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
