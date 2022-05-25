e100e62bcd894f4c64e2cebee433dbf2439e2ae2
Robson Sousa <robsoncartes@outlook.com>
2022-05-24 22:36:07 -0700

    'feat/class3': Show Score. Update 'src/components/MovieCard/index.tsx', 'src/components/MovieScore/index.tsx' and 'src/components/MovieStars/index.tsx' files.

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,6 +1,6 @@
 import MovieScore from "components/MovieScore";
 import {Link} from "react-router-dom";
-import {Movie} from "../../types/movie";
+import {Movie} from "types/movie";
 
 
 type Props = {
@@ -15,7 +15,7 @@ function MovieCard({movie}: Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore/>
+                <MovieScore count={movie.count} score={movie.score}></MovieScore>
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
